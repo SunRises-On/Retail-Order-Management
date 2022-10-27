a9fa7fcdf195ef44047569fa8f5a21593c2e959d
Jewelle Wilson <jewellelenicewilson@gmail.com>
2022-10-27 13:14:51 -0500

    Add insert function for image.

--- gift/gift/src/main/java/com/example/gift/insert/Main.java
+++ gift/gift/src/main/java/com/example/gift/insert/Main.java
@@ -15,24 +15,37 @@ public class Main {
 
     public static void main(String[] args) throws Exception{
         //Register the Driver
-        //Driver driver = new com.mysql.jdbc.Driver();
-        //Driver driver = new com.mysql.jdbc.Driver();
-        DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+        //DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
 
         //Getting the connection
-        String mysqlUrl = "jdbc:mysql://localhost:3306";
-        Connection con = DriverManager.getConnection(mysqlUrl, "root",
-                "password");
+        //String mysqlUrl = "jdbc:mysql://localhost:3306";
+        //Connection con = DriverManager.getConnection(mysqlUrl, "root","password");
+
+        Connection con = getConnection();
+        con.setAutoCommit(false);
+
         System.out.println("Connection established ....");
         PreparedStatement pstmt = con.prepareStatement(
-                "INSERT INTO sys.product VALUES (?,?)");
+                "INSERT INTO sys.product VALUES (?,?,?,?,?)");
 
         InputStream in = new FileInputStream(
-                "C:\\Users\\jewel\\Desktop\\jpg\\shirt.jpg"
+                "C:\\Retail-Order-Management\\gift\\gift\\src\\main\\resources\\jpg\\shirt.jpg"
         );
-        pstmt.setBlob(2,in);
+        pstmt.setInt(1,1);
+        pstmt.setInt(2,5);
+        pstmt.setBlob(3,in);
+        pstmt.setString(4,"t-shirt");
+        pstmt.setInt(5,1000);
         pstmt.execute();
         System.out.println("Record inserted....");
 
+    }
+
+    private static Connection getConnection() throws Exception{
+        //Register the Driver
+        DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+        String mysqlUrl = "jdbc:mysql://localhost:3306";
+
+        return DriverManager.getConnection(mysqlUrl, "root", "password");
     }
 }
